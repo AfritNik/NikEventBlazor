c77e615247f239e3ff1645f123eec4c3efac2641
oskik <oskik@DESKTOP-3KCEVNO>
2020-04-19 23:17:24 +0300

    Adding events page and controller

---
+++ NikEventBlazor/Server/Controllers/NikEventsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NikEventBlazor.Shared.Entries;
+
+namespace NikEventBlazor.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NikEventController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<NikEventController> logger;
+
+        public NikEventController(ILogger<NikEventController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<NikEvent> Get()
+        {
+            return new List<NikEvent>()
+            {
+                new NikEvent()
+                {
+                    Caption = "First event"
+                },
+                new NikEvent()
+                {
+                    Caption = "Second event"
+                }
+            };
+        }
+    }
+}
---
+++ NikEventBlazor/Shared/Entries/NikEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NikEventBlazor.Shared.Entries
+{
+    public class NikEvent
+    {
+        public int Id { get; set; }
+        public string Caption { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
